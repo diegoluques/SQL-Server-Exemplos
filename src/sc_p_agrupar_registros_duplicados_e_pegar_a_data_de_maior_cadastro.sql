1482f3a300e9f560a1897faaa6c3d220ec5180b0
Diego Luques <luquesdiego@outlook.com>
2019-10-22 12:03:42 -0400

    Agrupamento de registros

---
+++ src/sc_p_agrupar_registros_duplicados_e_pegar_a_data_de_maior_cadastro.sql
@@ -0,0 +1,29 @@
+/*
+	Abaixo tem 6 registros onde existe CNPJs duplicados mas com a data de cadastro diferente.
+	É necesspario extrair somente os CNPJs onde a data de cadastro é maior.
+
+	Resultado esperado:
+	cnpj				dataCadastro
+	00.000.000/0002-22	06/07/2011
+	00.000.000/0001-11	07/05/2017
+	00.000.000/0003-33	07/18/2019
+*/
+
+DECLARE @TB_TABELA AS TABLE (cnpj varchar(20), dataCadastro DATE)
+
+INSERT INTO @TB_TABELA 
+	VALUES  
+		('00.000.000/0001-11', '30/01/2014'),	
+		('00.000.000/0001-11', '05/07/2017'),	
+		('00.000.000/0002-22', '07/06/2011'),	
+		('00.000.000/0002-22', '05/06/2015'),	
+		('00.000.000/0003-33', '18/07/2019'),	
+		('00.000.000/0003-33', '08/07/2019') 
+
+SELECT 
+	 cnpj, 
+	 MAX(CONVERT(VARCHAR(10), dataCadastro, 101)) as dataCadastro
+
+FROM @TB_TABELA 
+GROUP BY cnpj
+ORDER BY dataCadastro ASC
