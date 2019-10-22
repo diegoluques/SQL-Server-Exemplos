c20e846d30f49437c460785fa79b420de53c9d10
Diego Luques <55838972+diegoluques@users.noreply.github.com>
2019-10-22 14:51:40 -0400

    Update sc_p_identificar_registro_que_nao_existe_em_outra_tabela.sql

--- src/sc_p_identificar_registro_que_nao_existe_em_outra_tabela.sql
+++ src/sc_p_identificar_registro_que_nao_existe_em_outra_tabela.sql
@@ -1,10 +1,10 @@
 /*
 	Abaixo tem duas tabelas A e B, na A tem 3 registros de CNPJs e 
 	na B tem dois registros repetidos na A. 
-	… necess·rio identificar qual CNPJ da tabela A n„o existe na tabela B.
+	√â necess√°rio identificar qual CNPJ da tabela A n√£o existe na tabela B.
 
 	Resultado esperado:
-	1-Apresentar o CNPJ 00000000000193 que n„o existe na tabela B
+	1-Apresentar o CNPJ 00000000000193 que n√£o existe na tabela B
 	CNPJ
 	00000000000193
 */
@@ -14,19 +14,19 @@
 
 	INSERT INTO @tb_CLIENTE_A 
 		VALUES  ('00000000000191'),
-				('00000000000192'),
-				('00000000000193')
+			('00000000000192'),
+			('00000000000193')
 	
 	INSERT INTO @tb_CLIENTE_B 
 		VALUES  ('00000000000191'),
-				('00000000000192') 
+			('00000000000192') 
 	
-	--Retorna o ˙nico CNPJ que n„o tem na tabela B (193)
+	--Retorna o √∫nico CNPJ que n√£o tem na tabela B (193)
 	SELECT * FROM @tb_CLIENTE_A 
 	EXCEPT 
 	SELECT * FROM @tb_CLIENTE_B
 
-	--N„o ir· retornar registos, pois os dois CNPJs j· existe na tabela A	 
+	--N√£o ir√° retornar registos, pois os dois CNPJs j√° existe na tabela A	 
 	SELECT * FROM @tb_CLIENTE_B
 	EXCEPT 
 	SELECT * FROM @tb_CLIENTE_A
