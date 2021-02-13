f595829759d21fa089a86101c4399afdd947656a
Diego Luques <luquesdiego@outlook.com>
2021-02-13 17:16:16 -0400

    Ajuste na formatação

--- src/15_gerar_propriedades_para_as_colunas_de_uma_tabela.sql
+++ src/15_gerar_propriedades_para_as_colunas_de_uma_tabela.sql
@@ -1,16 +1,16 @@
-SELECT	'public ' + 
-	case when DATA_TYPE = 'datetime' then 
-		'DateTime' 
-	when DATA_TYPE = 'bit' then 
-		'bool' 
-	when DATA_TYPE = 'int' then 
-		'int' 
-	when DATA_TYPE = 'decimal' then 
-		'decimal' 
-	when DATA_TYPE = 'varchar' then 
-		'string' 
-	end + ' ' + 
-	UPPER(LEFT(COLUMN_NAME,1))+SUBSTRING(COLUMN_NAME,2,LEN(COLUMN_NAME))
-	+ ' {get; set;}'  as propriedade
+SELECT	'public ' +
+		case when DATA_TYPE = 'datetime' then
+			'DateTime'
+		when DATA_TYPE = 'bit' then
+			'bool'
+		when DATA_TYPE = 'int' then
+			'int'
+		when DATA_TYPE = 'decimal' then
+			'decimal'
+		when DATA_TYPE = 'varchar' then
+			'string'
+		end + ' ' +
+		UPPER(LEFT(COLUMN_NAME,1))+SUBSTRING(COLUMN_NAME,2,LEN(COLUMN_NAME))
+		+ ' {get; set;}' as propriedade
 FROM	INFORMATION_SCHEMA.COLUMNS
 WHERE	TABLE_NAME = 'Movimento'
